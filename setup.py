1db61782cd9078b425bfb694c2e5eada7fb15153
CharlesPikachu <1159254961@qq.com>
2022-07-20 22:56:35 +0800

    update setup

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     license=videodl.__license__,
     include_package_data=True,
     entry_points={'console_scripts': ['videodl = videodl.videodl:videodlcmd']},
-    install_requires=list(open('requirements.txt', 'r').readlines()),
+    install_requires=[lab.strip('\n') for lab in list(open('requirements.txt', 'r').readlines())],
     zip_safe=True,
     packages=find_packages()
 )
