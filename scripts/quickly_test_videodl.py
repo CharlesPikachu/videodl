bd837c6976e53f5c69ee37f709daae9531bc8ddb
CharlesPikachu <1159254961@qq.com>
2025-11-21 11:30:47 +0800

    upload test examples

---
+++ scripts/quickly_test_videodl.py
@@ -0,0 +1,121 @@
+'''
+Function:
+    Implementation of quickly checking the effectiveness of videodl
+Author:
+    Zhenchao Jin
+WeChat Official Account (微信公众号):
+    Charles的皮卡丘
+'''
+
+
+
+TEST_EXAMPLES = {
+    'AcFunVideoClient': [
+        'https://www.acfun.cn/v/ac29566205', 
+        'https://www.acfun.cn/v/ac47998293', 
+        'https://www.acfun.cn/v/ac36491489'
+    ],
+    'HaokanVideoClient': [
+        'https://haokan.baidu.com/v?vid=7295039339702288421', 
+        'https://haokan.baidu.com/v?vid=7224830823778858146'
+    ],
+    'TedVideoClient': [
+        'https://www.ted.com/talks/alanna_shaikh_why_covid_19_is_hitting_us_now_and_how_to_prepare_for_the_next_outbreak', 
+        'https://www.ted.com/talks/adam_kucharski_how_can_we_control_the_coronavirus_pandemic'
+    ],
+    'PipigaoxiaoVideoClient': [
+        'https://h5.ippzone.com/pp/post/350259149175?zy_to=copy_link&share_count=1&m=0cd13da8548a1bc85813d8c60d331e22&app=&type=post&did=d2bddf23159ae495&mid=1270840711117&pid=350259149175',
+        'https://h5.ippzone.com/pp/post/870235406308?app=&did=d2bddf23159ae495&m=0cd13da8548a1bc85813d8c60d331e22&mid=1270840711117&pid=870235406308&share_count=1&type=post&zy_to=copy_link'
+    ],
+    'PipixVideoClient': [
+        'https://h5.pipix.com/item/6740623460659108107?app_id=1319&app=super&timestamp=1574241444&user_id=1085910221863021&carrier_region=cn&region=cn&language=zh&utm_source=weixin',
+        'https://h5.pipix.com/item/6863294377570081027?app_id=1319&app=super&timestamp=1598011674&user_id=62108092335&carrier_region=cn&region=cn&language=zh&utm_source=weixin'
+    ],
+    'Ku6VideoClient': [
+        'https://www.ku6.com/video/detail?id=McZoSe_hgG_jwzy7pQLqvMJ3IoI.', 
+        'https://www.ku6.com/video/detail?id=HE3lfhcp13Gd0qND4zfzXYQONMY.'
+    ],
+    'KuaishouVideoClient': [
+        'https://www.kuaishou.com/short-video/3xwzr5dveyqc5fa?authorId=3xv7d3j7hqqpksi', 
+        'https://www.kuaishou.com/short-video/3xjpwzyparcgnck?authorId=3xbbsmxr7cdmhqs'
+    ],
+    'BilibiliVideoClient': [
+        'https://www.bilibili.com/video/BV16Z4y1X784/?spm_id_from=333.851.b_7265636f6d6d656e64.2', 
+        'https://www.bilibili.com/video/BV1KZgHzJEs6/?spm_id_from=333.337.search-card.all.click'
+    ],
+    'YinyuetaiVideoClient': [
+        'https://www.yinyuetai.com/watch/7200480383631265792?listType=swiper&listId=6998475633361805312'
+    ],
+    'BaiduTiebaVideoClient': [
+        'https://tieba.baidu.com/p/6098286801', 
+        'https://tieba.baidu.com/p/7280373361'
+    ],
+    'MGTVVideoClient': [
+        'https://www.mgtv.com/l/100026064/19868457.html?fpa=1684&fpos=&lastp=ch_home&cpid=5',
+        'https://www.mgtv.com/b/788366/23780111.html?fpa=1756&fpos=&lastp=ch_home', # requires pass VIP login-in cookies by default_download_cookies to download full video
+        'https://www.mgtv.com/b/805972/23756299.html?fpa=1261&fpos=&lastp=ch_home&cpid=5', # requires pass login-in cookies by default_download_cookies to download full video
+    ],
+    'ZhihuVideoClient': [
+        'https://www.zhihu.com/question/38557752/answer/2280634747',
+        'https://www.miguvideo.com/p/detail/958634941?lastLocation=b1c29dcd91f94a0289bf6295140f43b2%23261fe51d763b4a86bb272aaa9de47b49',
+    ],
+    'OasisVideoClient': [
+        'https://m.oasis.weibo.cn/v1/h5/share?sid=4497689997350015&luicode=10001122&lfid=lz_qqfx&bid=4497689997350015',
+        'https://m.oasis.weibo.cn/v1/h5/share?sid=4506676592820518',
+    ],
+    'PearVideoClient': [
+        'https://www.pearvideo.com/video_1803264',
+        'https://www.pearvideo.com/video_1803574',
+    ],
+    'HuyaVideoClient': [
+        'https://www.huya.com/video/play/1084317636.html',
+        'https://www.huya.com/video/play/1081888422.html',
+    ],
+    'MeipaiVideoClient': [
+        'http://www.meipai.com/media/6995325250108940314',
+        'http://www.meipai.com/media/6983711035636086653'
+    ],
+    'DuxiaoshiVideoClient': [
+        'http://quanmin.baidu.com/sv?source=share-h5&pd=qm_share_search&vid=12474281128791424380', 
+        'https://mbd.baidu.com/newspage/data/videolanding?nid=sv_8388278490910791594&sourceFrom=rec'
+    ],
+    'SixRoomVideoClient': [
+        'https://v.6.cn/minivideo/7425678',
+        'https://v.6.cn/profile/watchMini.php?vid=5584111', 
+    ],
+    'WeishiVideoClient': [
+        'https://h5.weishi.qq.com/weishi/feed/76EaWNkEF1IqtfYVH/',
+        'https://isee.weishi.qq.com/ws/app-pages/share/index.html?wxplay=1&id=7siJelmUp1MkDSPeo&spid=3704775550396963513&qua=v1_and_weishi_8.81.0_590_312026001_d&chid=100081014&pkg=3670&attach=cp_reserves3_1000370011'
+    ],
+    'ZuiyouVideoClient': [
+        'https://share.xiaochuankeji.cn/hybrid/share/post?pid=285701565&zy_to=applink&share_count=1&m=01a529af83555742c8f3b3dd43458057&d=9d65722d31131635796498e5eefbb375256c002df068298ce57006de10ed92fd&app=zuiyou&recommend=r0&name=n0&title_type=t0',
+        'https://share.xiaochuankeji.cn/hybrid/share/post?pid=413455067'
+    ],
+    'XinpianchangVideoClient': [
+        'https://www.xinpianchang.com/a13536060?from=IndexPick&part=%E7%BC%96%E8%BE%91%E7%B2%BE%E9%80%89&index=1',
+        'https://www.xinpianchang.com/a13419465?from=ArticleList'
+    ],
+    'WeSingVideoClient': [
+        'https://kg.qq.com/node/user/bb132c338e/song/play-edLkcwAsRj?s=bCyoDlbCUhcjXbkQ&shareuid=&topsource=znxvljkwehoit_rqojkwehfguioqef_fnajkgfb&g_f=',
+        'https://kg.qq.com/node/play?s=WYsF1AWj1UTvLWXu&g_f=personal&appsource=&pageId=personalH5'
+    ],
+    'XiguaVideoClient': [
+        'https://www.ixigua.com/7382121243505328655',
+        'https://m.ixigua.com/video/7405525381257626162?logTag=93d90688ada741c34133'
+    ],
+    'WeiboVideoClient': [
+        'https://weibo.com/tv/show/1034:5234817776943232?mid=5234851004547318',
+        'https://m.weibo.cn/detail/5234820306442377',
+        'https://m.weibo.cn/detail/4749967266939049',
+        'https://weibo.com/tv/v/HApWK8FAc?fid=1034:4386795211940756',
+    ],
+    'RednoteVideoClient': [
+        'http://xhslink.com/o/6B9wstL9kEM',
+        'https://www.xiaohongshu.com/explore/691b27510000000007017708?app_platform=android&ignoreEngage=true&app_version=9.8.1&share_from_user_hidden=true&xsec_source=app_share&type=video&xsec_token=CBA-xMhkMF_MswlwR4_gmUDZfWCILfe7lOh0MNcTIvzIo=&author_share=1&xhsshare=&shareRedId=Nz02QUk7OEs-OUpFPEE3TEc7TEk3Pjw_&apptime=1763572146&share_id=c4d5f5d382684815924cc6a6843486e3&share_channel=wechat'
+    ],
+    'CCTVVideoClient': [
+        'https://v.cctv.com/2021/06/05/VIDEwn0n7VRJokIL7rBi2ink210605.shtml?spm=C90324.Pfdd0SYeqktv.Eri5TUDwaTXO.6',
+        'https://tv.cctv.com/2022/04/15/VIDEYQOwJ33dIqm1h9PvmIpO220415.shtml?spm=C95797228340.P3XSH3djsU2B.0.0',
+        'https://xczx.cctv.com/2025/11/19/ARTIhGNpDUfXpmRX0qjQ6mSC251119.shtml?spm=C73274.Pt2UfsofZQQ5.EmwSKtAQYeDI.2',
+    ]
+}
