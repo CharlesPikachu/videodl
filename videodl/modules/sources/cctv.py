1f5a25461b1ac52ab2d4652becb174312c64461e
CharlesPikachu <1159254961@qq.com>
2025-12-11 19:45:33 +0800

    fix cctv download error

--- videodl/modules/sources/cctv.py
+++ videodl/modules/sources/cctv.py
@@ -25,9 +25,7 @@ class CCTVVideoClient(BaseVideoClient):
         self.default_parse_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
         }
-        self.default_download_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-        }
+        self.default_download_headers = {}
         self.default_headers = self.default_parse_headers
         self._initsession()
     '''parsefromurl'''
