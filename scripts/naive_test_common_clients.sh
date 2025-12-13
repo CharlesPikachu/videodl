660a3fbfca99d5f88774788e589640d67d68fe6b
CharlesPikachu <1159254961@qq.com>
2025-12-13 22:15:33 +0800

    update scripts

--- scripts/naive_test_common_clients.sh
+++ scripts/naive_test_common_clients.sh
@@ -1,22 +1,22 @@
 # IIILabVideoClient
-videodl -i "https://www.le.com/ptv/vplay/77916175.html#vid=77916175" -g -c IIILabVideoClient
+videodl -i "https://www.le.com/ptv/vplay/77916175.html#vid=77916175" -g -a IIILabVideoClient
 # KedouVideoClient
-videodl -i "https://www.douyin.com/jingxuan?modal_id=7578412593719577899" -g -c KedouVideoClient
+videodl -i "https://www.douyin.com/jingxuan?modal_id=7578412593719577899" -g -a KedouVideoClient
 # SnapAnyVideoClient
-videodl -i "https://www.youtube.com/watch?v=uZ8rxvAoFuc&list=RDuZ8rxvAoFuc&start_radio=1" -g -c SnapAnyVideoClient
+videodl -i "https://www.youtube.com/watch?v=uZ8rxvAoFuc&list=RDuZ8rxvAoFuc&start_radio=1" -g -a SnapAnyVideoClient
 # GVVIPVideoClient
-videodl -i "https://v.qq.com/x/cover/mzc00200ss1zz0x/r4101dsk6f5.html?cut_vid=p41012zzjzo&scene_id=3&start=1455" -g -c GVVIPVideoClient
+videodl -i "https://v.qq.com/x/cover/mzc00200ss1zz0x/r4101dsk6f5.html?cut_vid=p41012zzjzo&scene_id=3&start=1455" -g -a GVVIPVideoClient
 # VgetVideoClient
-videodl -i "https://x.com/iluminatibot/status/1996651394963734976" -g -c VgetVideoClient
+videodl -i "https://x.com/iluminatibot/status/1996651394963734976" -g -a VgetVideoClient
 # ILoveAPIVideoClient
-videodl -i "'http://peiyinxiu.com/m/127066455" -g -c ILoveAPIVideoClient
+videodl -i "'http://peiyinxiu.com/m/127066455" -g -a ILoveAPIVideoClient
 # XMFlvVideoClient
-videodl -i "https://v.qq.com/x/cover/mzc00200tpgbo78/y4101ca2mcp.html" -g -c XMFlvVideoClient
+videodl -i "https://v.qq.com/x/cover/mzc00200tpgbo78/y4101ca2mcp.html" -g -a XMFlvVideoClient
 # QZXDPToolsVideoClient
-videodl -i "https://www.bilibili.com/video/BV1uymuBcE6D/?spm_id_from=333.1007.tianma.1-2-2.click" -g -c QZXDPToolsVideoClient
+videodl -i "https://www.bilibili.com/video/BV1uymuBcE6D/?spm_id_from=333.1007.tianma.1-2-2.click" -g -a QZXDPToolsVideoClient
 # KuKuToolsVideoClient
-videodl -i "https://www.xiaohongshu.com/explore/6898415f000000000403dc3b?xsec_token=ABozZxFfnIo4Jum5RDrI97yLjwD0ng6sc5_55zhZGU4gQ=&xsec_source=pc_user" -g -c KuKuToolsVideoClient
+videodl -i "https://www.xiaohongshu.com/explore/6898415f000000000403dc3b?xsec_token=ABozZxFfnIo4Jum5RDrI97yLjwD0ng6sc5_55zhZGU4gQ=&xsec_source=pc_user" -g -a KuKuToolsVideoClient
 # YouChuangVideoClient
-videodl -i "https://m.immomo.com/s/moment/new-share-v2/ar8198227777.html?time=1580294283&name=LpBAX+1lhEzqgkLc/ICG1w==&avatar=036495A9-5988-B90B-0CFA-C2415EAC1A3E20190912&isdaren=0&isuploader=1&from=weibo" -g -c YouChuangVideoClient
+videodl -i "https://m.immomo.com/s/moment/new-share-v2/ar8198227777.html?time=1580294283&name=LpBAX+1lhEzqgkLc/ICG1w==&avatar=036495A9-5988-B90B-0CFA-a2415EAC1A3E20190912&isdaren=0&isuploader=1&from=weibo" -g -a YouChuangVideoClient
 # LongZhuVideoClient
-videodl -i "https://www.toutiao.com/video/7358202402865938953/?log_from=24fdd11d4e4e68_1765499453711" -g -c LongZhuVideoClient
+videodl -i "https://www.toutiao.com/video/7358202402865938953/?log_from=24fdd11d4e4e68_1765499453711" -g -a LongZhuVideoClient
