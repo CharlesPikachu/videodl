dd85d1cdb5264bb96300d8f4c57d50b75e9a415a
CharlesPikachu <1159254961@qq.com>
2025-12-13 22:14:27 +0800

    update scripts

---
+++ scripts/naive_test_common_clients.sh
@@ -0,0 +1,22 @@
+# IIILabVideoClient
+videodl -i "https://www.le.com/ptv/vplay/77916175.html#vid=77916175" -g -c IIILabVideoClient
+# KedouVideoClient
+videodl -i "https://www.douyin.com/jingxuan?modal_id=7578412593719577899" -g -c KedouVideoClient
+# SnapAnyVideoClient
+videodl -i "https://www.youtube.com/watch?v=uZ8rxvAoFuc&list=RDuZ8rxvAoFuc&start_radio=1" -g -c SnapAnyVideoClient
+# GVVIPVideoClient
+videodl -i "https://v.qq.com/x/cover/mzc00200ss1zz0x/r4101dsk6f5.html?cut_vid=p41012zzjzo&scene_id=3&start=1455" -g -c GVVIPVideoClient
+# VgetVideoClient
+videodl -i "https://x.com/iluminatibot/status/1996651394963734976" -g -c VgetVideoClient
+# ILoveAPIVideoClient
+videodl -i "'http://peiyinxiu.com/m/127066455" -g -c ILoveAPIVideoClient
+# XMFlvVideoClient
+videodl -i "https://v.qq.com/x/cover/mzc00200tpgbo78/y4101ca2mcp.html" -g -c XMFlvVideoClient
+# QZXDPToolsVideoClient
+videodl -i "https://www.bilibili.com/video/BV1uymuBcE6D/?spm_id_from=333.1007.tianma.1-2-2.click" -g -c QZXDPToolsVideoClient
+# KuKuToolsVideoClient
+videodl -i "https://www.xiaohongshu.com/explore/6898415f000000000403dc3b?xsec_token=ABozZxFfnIo4Jum5RDrI97yLjwD0ng6sc5_55zhZGU4gQ=&xsec_source=pc_user" -g -c KuKuToolsVideoClient
+# YouChuangVideoClient
+videodl -i "https://m.immomo.com/s/moment/new-share-v2/ar8198227777.html?time=1580294283&name=LpBAX+1lhEzqgkLc/ICG1w==&avatar=036495A9-5988-B90B-0CFA-C2415EAC1A3E20190912&isdaren=0&isuploader=1&from=weibo" -g -c YouChuangVideoClient
+# LongZhuVideoClient
+videodl -i "https://www.toutiao.com/video/7358202402865938953/?log_from=24fdd11d4e4e68_1765499453711" -g -c LongZhuVideoClient
