7f237ad1a2fa85048d93959c141457ea51a86a64
CharlesPikachu <1159254961@qq.com>
2026-01-01 07:47:30 +0800

    bump to videofetch 0.4.5

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 project = 'videodl'
 copyright = '2021-2030, Zhenchao Jin'
 author = 'Zhenchao Jin'
-release = '0.4.4'
+release = '0.4.5'
 
 # -- General configuration ---------------------------------------------------
 
