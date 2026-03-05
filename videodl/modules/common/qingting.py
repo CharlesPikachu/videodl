3db474884313472547484ea0a4a50689a6fb01ff
CharlesPikachu <1159254961@qq.com>
2026-03-05 14:22:51 +0800

    supplement cover info for some supported sites

--- videodl/modules/common/qingting.py
+++ videodl/modules/common/qingting.py
@@ -11,21 +11,16 @@ import copy
 from ..sources import BaseVideoClient
 from ..utils import RandomIPGenerator
 from ..utils.domains import platformfromurl
-from ..utils import VideoInfo, FileTypeSniffer, useparseheaderscookies, legalizestring, resp2json, yieldtimerelatedtitle, optionalimport
+from ..utils import VideoInfo, FileTypeSniffer, useparseheaderscookies, legalizestring, resp2json, yieldtimerelatedtitle, safeextractfromdict
 
 
 '''QingtingVideoClient'''
 class QingtingVideoClient(BaseVideoClient):
     source = 'QingtingVideoClient'
     def __init__(self, **kwargs):
-        if ('enable_parse_curl_cffi' not in kwargs) and optionalimport('curl_cffi'): kwargs['enable_parse_curl_cffi'] = True
         super(QingtingVideoClient, self).__init__(**kwargs)
-        self.default_parse_headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
-        }
-        self.default_download_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-        }
+        self.default_parse_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
+        self.default_download_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
         self.default_headers = self.default_parse_headers
         self._initsession()
     '''parsefromurl'''
@@ -33,7 +28,7 @@ class QingtingVideoClient(BaseVideoClient):
     def parsefromurl(self, url: str, request_overrides: dict = None):
         # prepare
         request_overrides = request_overrides or {}
-        video_info = VideoInfo(source=self.source)
+        video_info = VideoInfo(source=self.source, enable_nm3u8dlre=False, download_with_ffmpeg=True) if BaseVideoClient.belongto(url, {"ted.com", "xinpianchang.com", "ifeng.com"}) else VideoInfo(source=self.source, enable_nm3u8dlre=True)
         null_backup_title = yieldtimerelatedtitle(self.source)
         if platformfromurl(url) in {'bilibili'}: video_info.update(dict(default_download_headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'Referer': 'https://www.bilibili.com/'}))
         # try parse
@@ -42,28 +37,23 @@ class QingtingVideoClient(BaseVideoClient):
             # --get request
             headers = copy.deepcopy(self.default_headers)
             RandomIPGenerator().addrandomipv4toheaders(headers)
-            if "douyin.com" in url: resp = self.get(f'https://api.33tool.com/api/parse/douyin?url={url}', headers=headers, **request_overrides)
-            elif "bilibili.com" in url or "b23.tv" in url: resp = self.get(f'https://api.33tool.com/api/parse/bilibili?url={url}', headers=headers, **request_overrides)
-            elif "xiaohongshu.com" in url or "xhslink.com" in url: resp = self.get(f'https://api.33tool.com/api/parse/redbook?url={url}', headers=headers, **request_overrides)
-            elif "kuaishou.com" in url: resp = self.get(f'https://api.33tool.com/api/parse/kuaishou?url={url}', headers=headers, **request_overrides)
-            else: raise NotImplementedError()
-            resp.raise_for_status()
+            if "douyin.com" in url: (resp := self.get(f'https://api.33tool.com/api/parse/douyin?url={url}', headers=headers, **request_overrides)).raise_for_status()
+            elif "bilibili.com" in url or "b23.tv" in url: (resp := self.get(f'https://api.33tool.com/api/parse/bilibili?url={url}', headers=headers, **request_overrides)).raise_for_status()
+            elif "xiaohongshu.com" in url or "xhslink.com" in url: (resp := self.get(f'https://api.33tool.com/api/parse/redbook?url={url}', headers=headers, **request_overrides)).raise_for_status()
+            elif "kuaishou.com" in url: (resp := self.get(f'https://api.33tool.com/api/parse/kuaishou?url={url}', headers=headers, **request_overrides)).raise_for_status()
+            else: raise NotImplementedError(f"{self.source} can only parse 'douyin', 'bilibili', 'xiaohongshu' and 'kuaishou'")
             raw_data = resp2json(resp=resp)
             video_info.update(dict(raw_data=raw_data))
             # --video title
-            video_title = legalizestring(raw_data['data'].get('title') or null_backup_title, replace_null_string=null_backup_title).removesuffix('.')
+            video_title = legalizestring(safeextractfromdict(raw_data, ['data', 'title'], None) or null_backup_title, replace_null_string=null_backup_title).removesuffix('.')
             # --download url
             download_url = raw_data['data']['videoUrl']
             video_info.update(dict(download_url=download_url))
             # --other infos
-            guess_video_ext_result = FileTypeSniffer.getfileextensionfromurl(
-                url=download_url, headers=self.default_download_headers, request_overrides=request_overrides, cookies=self.default_download_cookies,
-            )
+            guess_video_ext_result = FileTypeSniffer.getfileextensionfromurl(url=download_url, headers=self.default_download_headers, request_overrides=request_overrides, cookies=self.default_download_cookies)
             ext = guess_video_ext_result['ext'] if guess_video_ext_result['ext'] and guess_video_ext_result['ext'] != 'NULL' else video_info['ext']
             if ext in {'xsl'}: ext = 'mp4'
-            video_info.update(dict(
-                title=video_title, file_path=os.path.join(self.work_dir, self.source, f'{video_title}.{ext}'), ext=ext, guess_video_ext_result=guess_video_ext_result, identifier=video_title,
-            ))
+            video_info.update(dict(title=video_title, file_path=os.path.join(self.work_dir, self.source, f'{video_title}.{ext}'), ext=ext, guess_video_ext_result=guess_video_ext_result, identifier=video_title, cover_url=safeextractfromdict(raw_data, ['data', 'coverUrl'], None)))
             video_infos.append(video_info)
         except Exception as err:
             err_msg = f'{self.source}.parsefromurl >>> {url} (Error: {err})'
--- videodl/modules/common/qzxdptools.py
+++ videodl/modules/common/qzxdptools.py
@@ -11,7 +11,7 @@ import copy
 from ..sources import BaseVideoClient
 from ..utils import RandomIPGenerator
 from ..utils.domains import platformfromurl
-from ..utils import VideoInfo, FileTypeSniffer, useparseheaderscookies, legalizestring, resp2json, yieldtimerelatedtitle
+from ..utils import VideoInfo, FileTypeSniffer, useparseheaderscookies, legalizestring, resp2json, yieldtimerelatedtitle, safeextractfromdict
 
 
 '''QZXDPToolsVideoClient'''
@@ -20,15 +20,10 @@ class QZXDPToolsVideoClient(BaseVideoClient):
     def __init__(self, **kwargs):
         super(QZXDPToolsVideoClient, self).__init__(**kwargs)
         self.default_parse_headers = {
-            "host": "tools.qzxdp.cn", "origin": "https://tools.qzxdp.cn", "referer": "https://tools.qzxdp.cn/video_spider",
-            "sec-ch-ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"', "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest",
+            "host": "tools.qzxdp.cn", "origin": "https://tools.qzxdp.cn", "referer": "https://tools.qzxdp.cn/video_spider", "sec-ch-ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"', "sec-ch-ua-mobile": "?0", "x-requested-with": "XMLHttpRequest",
+            "sec-ch-ua-platform": "\"Windows\"", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
         }
-        self.default_download_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-        }
+        self.default_download_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'}
         self.default_headers = self.default_parse_headers
         self._initsession()
     '''parsefromurl'''
@@ -36,7 +31,7 @@ class QZXDPToolsVideoClient(BaseVideoClient):
     def parsefromurl(self, url: str, request_overrides: dict = None):
         # prepare
         request_overrides = request_overrides or {}
-        video_info = VideoInfo(source=self.source)
+        video_info = VideoInfo(source=self.source, enable_nm3u8dlre=False, download_with_ffmpeg=True) if BaseVideoClient.belongto(url, {"ted.com", "xinpianchang.com", "ifeng.com"}) else VideoInfo(source=self.source, enable_nm3u8dlre=True)
         null_backup_title = yieldtimerelatedtitle(self.source)
         if platformfromurl(url) in {'bilibili'}: video_info.update(dict(default_download_headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'Referer': 'https://www.bilibili.com/'}))
         # try parse
@@ -45,22 +40,17 @@ class QZXDPToolsVideoClient(BaseVideoClient):
             # --post request
             headers = copy.deepcopy(self.default_headers)
             RandomIPGenerator().addrandomipv4toheaders(headers)
-            resp = self.post(f'https://tools.qzxdp.cn/api/video_spider/query', data={'video_url': url}, headers=headers, **request_overrides)
-            resp.raise_for_status()
+            (resp := self.post(f'https://tools.qzxdp.cn/api/video_spider/query', data={'video_url': url}, headers=headers, **request_overrides)).raise_for_status()
             raw_data = resp2json(resp=resp)
             video_info.update(dict(raw_data=raw_data))
             # --video title
-            video_title = legalizestring(raw_data['data'].get('title') or null_backup_title, replace_null_string=null_backup_title).removesuffix('.')
+            video_title = legalizestring(safeextractfromdict(raw_data, ['data', 'title'], None) or null_backup_title, replace_null_string=null_backup_title).removesuffix('.')
             # --download url
             video_info.update(dict(download_url=raw_data['data']['url']))
             # --other infos
-            guess_video_ext_result = FileTypeSniffer.getfileextensionfromurl(
-                url=raw_data['data']['url'], headers=self.default_download_headers, request_overrides=request_overrides, cookies=self.default_download_cookies,
-            )
+            guess_video_ext_result = FileTypeSniffer.getfileextensionfromurl(url=raw_data['data']['url'], headers=self.default_download_headers, request_overrides=request_overrides, cookies=self.default_download_cookies)
             ext = guess_video_ext_result['ext'] if guess_video_ext_result['ext'] and guess_video_ext_result['ext'] != 'NULL' else video_info['ext']
-            video_info.update(dict(
-                title=video_title, file_path=os.path.join(self.work_dir, self.source, f'{video_title}.{ext}'), ext=ext, guess_video_ext_result=guess_video_ext_result, identifier=video_title,
-            ))
+            video_info.update(dict(title=video_title, file_path=os.path.join(self.work_dir, self.source, f'{video_title}.{ext}'), ext=ext, guess_video_ext_result=guess_video_ext_result, identifier=video_title, cover_url=safeextractfromdict(raw_data, ['data', 'cover'], None)))
             video_infos.append(video_info)
         except Exception as err:
             err_msg = f'{self.source}.parsefromurl >>> {url} (Error: {err})'
